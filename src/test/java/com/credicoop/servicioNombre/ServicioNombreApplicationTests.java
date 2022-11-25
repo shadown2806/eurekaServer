64412d9545ea304d5669aff17c4fa30ef8cb60bd
unknown <velazquezmathias280600@gmail.com>
2022-11-25 20:01:38 -0300

    Eureka Server

---
+++ src/test/java/com/credicoop/servicioNombre/ServicioNombreApplicationTests.java
@@ -0,0 +1,13 @@
+package com.credicoop.servicioNombre;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ServicioNombreApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
